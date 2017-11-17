7c2a2a0056ad598a8db0d089aae504a7caf42e7e
Grymbel <grymb3l@gmail.com>
2017-11-17 10:26:13 +0800

    Commits

--- PotHub/src/admin/AdminPanel.java
+++ PotHub/src/admin/AdminPanel.java
@@ -28,7 +28,6 @@ public class AdminPanel extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter pw = response.getWriter();
 		pw.append("<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>"
 		+ "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>"
