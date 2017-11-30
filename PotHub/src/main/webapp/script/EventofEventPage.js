81129c3dff33297c7f81604fe9cb20788bd4cb13
WongWeiXuan <wong_weixuan@hotmail.com>
2017-12-01 00:43:19 +0800

    Finishing EventofEventPage
    -Added js
    -Fixed css

---
+++ PotHub/src/main/webapp/script/EventofEventPage.js
@@ -0,0 +1,29 @@
+function initMap() {
+	var compassOne = {lat: 1.392, lng: 103.895};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 16,
+		center: compassOne
+	});
+	var marker = new google.maps.Marker({
+		position: compassOne,
+		map: map
+	});
+}
+
+function redirectToGoogle(){
+	window.open("http://maps.google.com/?ll=1.392,103.89", "_blank");
+}
+
+function changeColor(button){
+	if(button.innerHTML == "Join"){
+		button.classList.add('btn-warning');
+		button.classList.remove('btn-success');
+		button.innerHTML = "Pending";
+	}
+	else if(button.innerHTML == "Pending"){
+		button.classList.add('btn-success');
+		button.classList.remove('btn-warning');
+		button.innerHTML = "Join";
+	}
+	
+}
