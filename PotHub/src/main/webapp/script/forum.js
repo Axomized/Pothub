bee07435e040e31646e86494be4b6f03fc5718df
LimXuanZheng <limxuanzheng888@gmail.com>
2017-12-01 01:46:42 +0800

    Almost done with html and css of forum

---
+++ PotHub/src/main/webapp/script/forum.js
@@ -0,0 +1,53 @@
+
+
+function upfirst(){
+	var h = document.getElementById("firstcount").innerHTML;
+	var p = parseInt(h);
+	var i = (p + 1);
+	document.getElementById("firstcount").innerHTML = i;
+}
+
+function downfirst(){
+	var h = document.getElementById("firstcount").innerHTML;
+	var i = (h-1);
+	document.getElementById("firstcount").innerHTML = i;
+}
+
+function upsecond(){
+	var h = document.getElementById("secondcount").innerHTML;
+	var p = parseInt(h);
+	var i = (p + 1);
+	document.getElementById("secondcount").innerHTML = i;
+}
+
+function downsecond(){
+	var h = document.getElementById("secondcount").innerHTML;
+	var i = (h-1);
+	document.getElementById("secondcount").innerHTML = i;
+}
+
+function upthird(){
+	var h = document.getElementById("thirdcount").innerHTML;
+	var p = parseInt(h);
+	var i = (p + 1);
+	document.getElementById("thirdcount").innerHTML = i;
+}
+
+function downthird(){
+	var h = document.getElementById("thirdcount").innerHTML;
+	var i = (h-1);
+	document.getElementById("thirdcount").innerHTML = i;
+}
+
+function showsth(){
+	document.getElementById("overlay").style.display = "block";
+}
+
+function cancell(){
+	document.getElementById("overlay").style.display = "none";
+}
+
+function success(){
+	alert("Success!");
+	document.getElementById("overlay").style.display = "none";
+}
