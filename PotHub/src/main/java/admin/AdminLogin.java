8a30f10447d6069e35e314ac43050b6c6a51da5b
Grymbel <grymb3l@gmail.com>
2017-11-30 23:08:00 +0800

    Made an admin Login

---
+++ PotHub/src/main/java/admin/AdminLogin.java
@@ -0,0 +1,95 @@
+package admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Forum
+ */
+@WebServlet("/AdminLogin")
+public class AdminLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AdminLogin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.append("<!DOCTYPE html>"
+				+ "<html>"
+				+ "	<head>"
+				+ "		<meta charset='UTF-8'>"
+				+ "		<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"
+				+ "		<!-- Favicon -->"
+				+ "		<link rel='icon' href='https://localhost/PotHub/images/crab.gif' type='image/gif'>"
+				+ "		<link rel='icon' href='https://localhost/PotHub/images/crab.png?v=2' type='image/x-icon'>"
+				+ "		<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>"
+				+ "		<!-- Page Title -->"
+				+ "		<title>Login Page</title>"
+				+ "		<!-- Latest compiled and CSS -->"
+				+ "		<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' integrity='sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ' crossorigin='anonymous'>"
+				+ "		<!-- Optional theme -->"
+				+ "		<script src='https://use.fontawesome.com/aff6d7353c.js'></script>"
+				+ "		<!-- My Own Script -->"
+				+ "		<!-- <script src='script/'></script> -->"
+				+ "		<!-- My Style Sheet -->"
+				+ "		<link rel='stylesheet' type='text/css' href='css/LoginPage.css' />"
+				+ "	</head>"
+				+ "	<body>"
+				+ "		<div class='container'>"
+				+ "	<img src='images/PotHubLogo.jpg'>"
+				+ "	<h2>Sign In</h2>"
+				+ "			<div class='form-input'>"
+				+ "				<span><i class='fa fa-envelope fa-lg' aria-hidden='true'></i></span>"
+				+ "				<input type='text' name='username'"
+				+ "				placeholder='Enter Email'>"
+				+ "			</div>"
+				+ "			<div class ='form-input'>"
+				+ "				<span><i class='fa fa-unlock-alt fa-2x' aria-hidden='true'></i></span>"
+				+ "				<input type='password' name='password'"
+				+ "				placeholder='Enter Password'>"
+				+ "			</div>"
+				+ "			<div class='form-input'>"
+				+ "				<div class='login-button'>"
+				+ "                 <a href='AdminGeneral'>"
+				+ "						<input type='submit'>"
+				+ "                 </a>"
+				+ "				</div>"
+				+ "			</div>"
+				+ "		<div class='form-input'>"
+				+ "		<a href='#'>Create Account</a>"
+				+ "		<a href='#'>Forget Password</a>"
+				+ "		</div>"
+				+ "	</div>"
+				+ "		<script src='https://code.jquery.com/jquery-3.1.1.slim.min.js' integrity='sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n' crossorigin='anonymous'></script>"
+				+ "		<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js' integrity='sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb' crossorigin='anonymous'></script>"
+				+ "		<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js' integrity='sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn' crossorigin='anonymous'></script>"
+				+ "	</body>"
+				+ "</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
