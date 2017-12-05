6d4c87b7876f7555c4674f6e6aa19dc408f32cfd
Grymbel <grymb3l@gmail.com>
2017-12-06 02:05:37 +0800

    Report button

--- PotHub/src/main/webapp/script/p2pdetail.js
+++ PotHub/src/main/webapp/script/p2pdetail.js
@@ -5,3 +5,13 @@ function showProfileDropdown() {
 function hideProfileDropdown() {
 	document.getElementById("profileDropdownDiv").style.display = "none";
 }
+
+function showReportables() {
+	console.log("Showing reportables");
+	document.getElementById("reportList").style.display = "block";
+}
+
+function hideReportables() {
+	document.getElementById("reportList").style.display = "none";
+	console.log("Hiding reportables");
+}
