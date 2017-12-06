27528d8f4783dcada5a45bb7ecdf47788f608dbb
Axomized <dr.que9@gmail.com>
2017-12-07 01:28:56 +0800

    More edits done to Profile pages

---
+++ PotHub/src/main/java/donation/ConfirmDonation.java
@@ -0,0 +1,36 @@
+package donation;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConfirmDonation
+ */
+public class ConfirmDonation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConfirmDonation() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- PotHub/src/main/webapp/script/Profile.js
+++ PotHub/src/main/webapp/script/Profile.js
@@ -1,3 +1,15 @@
+function initMap() {
+	var address = {lat: 1.3787, lng: 103.8493};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 16,
+		center: address
+	});
+	var marker = new google.maps.Marker({
+		position: address,
+		map: map
+	});
+}
+
 function showProfileDropdown() {
 	document.getElementById("profileDropdownDiv").style.display = "block";
 }
