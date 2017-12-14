3b4f7ed25e776d5a38c967d2f212041a1609fc35
LimXuanZheng <limxuanzheng888@gmail.com>
2017-12-14 22:17:37 +0800

    Fixed some errors

--- PotHub/src/main/webapp/script/createNewPost.js
+++ PotHub/src/main/webapp/script/createNewPost.js
@@ -5,3 +5,51 @@ function showProfileDropdown() {
 function hideProfileDropdown() {
 	document.getElementById("profileDropdownDiv").style.display = "none";
 }
+
+function submitting(){
+	location.href = "Forum";
+}
+
+function goback(){
+	location.href = "Forum";
+}
+
+function showT(){
+	document.getElementById("text1").style.display = "block";
+}
+
+function showP(){
+	document.getElementById("image1").style.display = "block";
+}
+
+function showV(){
+	document.getElementById("video1").style.display = "block";
+}
+
+function showU(){
+	document.getElementById("link1").style.display = "block";
+}
+
+function showF(){
+	document.getElementById("file1").style.display = "block";
+}
+
+function closeT(){
+	document.getElementById("text1").style.display = "none";
+}
+
+function closeI(){
+	document.getElementById("image1").style.display = "none";
+}
+
+function closeV(){
+	document.getElementById("video1").style.display = "none";
+}
+
+function closeU(){
+	document.getElementById("link1").style.display = "none";
+}
+
+function closeF(){
+	document.getElementById("file1").style.display = "none";
+}
