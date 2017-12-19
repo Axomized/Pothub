19f089d84542982c2b22924e146f92bf15e244ee
xiangjing98 <xiangjing90@hotmail.com>
2017-12-19 21:21:05 +0800

    added servlet

---
+++ PotHub/src/main/java/login/Registration.java
@@ -0,0 +1,61 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Registration2
+ */
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Registration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		try(PrintWriter out = response.getWriter()){
+			
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String confirmPassword = request.getParameter("password2");
+			String name = request.getParameter("name");
+			String contact = request.getParameter("contact");
+			String address = request.getParameter("address");
+			
+			List errorList = new LinkedList();
+			
+			if(name.length() == 0){
+				errorList.add("Enter a name...");
+			}
+			
+		}catch (Exception e){
+			
+		}
+	}
+
+}
