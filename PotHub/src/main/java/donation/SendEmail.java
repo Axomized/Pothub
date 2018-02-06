dbb9222e2851408f0c295e93a3211c5a24cab616
Axomized <dr.que9@gmail.com>
2018-02-06 23:32:49 +0800

    Two types

--- PotHub/src/main/java/donation/SendEmail.java
+++ PotHub/src/main/java/donation/SendEmail.java
@@ -20,6 +20,7 @@ public class SendEmail {
 	private static final String SMTP_AUTH_USER = "azure_549896266f26c56c966686e0317ab9d1@azure.com";
 	private static final String SMTP_AUTH_PWD = "password123";
 
+	//For sending email by SendGrid
 	public void sendEmail(String receipientEmail, String content) throws MessagingException {
 		String to = receipientEmail;
 		String from = "pothubaspj@gmail.com";
@@ -55,4 +56,40 @@ public class SendEmail {
 		   return new PasswordAuthentication(username, password);
 		}
 	}
+	
+	/*
+	 * For sending email on localhost
+	 * 
+	 public void sendEmail(String receipientEmail, String content) {
+		String to = receipientEmail;
+		String from = "pothubaspj@gmail.com";
+		final String username = "pothubaspj@gmail.com";
+		final String password = "ITP292-03";
+		String host = "smtp.gmail.com";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+				}
+		});
+		
+		try {
+	         MimeMessage message = new MimeMessage(session);
+	         message.setFrom(new InternetAddress(from));
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	         message.setSubject("PotHub Donation PIN");
+	         message.setText("This is the PIN to confirm your donation. It is valid only for 5 minutes.\n\n" + content);
+	         Transport.send(message);
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+	}
+	 */
 }
